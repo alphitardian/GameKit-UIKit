2dc37d426438cb495a0ca0c1eec2ba20c81d75b0
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2022-08-25 21:27:56 +0700

    First commit

---
+++ GameCenterUIKit/GameCenterHelper.swift
@@ -0,0 +1,82 @@
+//
+//  GameCenterHelper.swift
+//  GameCenterUIKit
+//
+//  Created by Ardian Pramudya Alphita on 23/08/22.
+//
+
+import GameKit
+
+protocol GameCenterHelperDelegate {
+    func presentAuth(_ gameCenter: GameCenterHelper, viewController: UIViewController?)
+    func presentMatchmaker(_ gameCenter: GameCenterHelper, viewController: UIViewController?)
+    func presentGame(_ gameCenter: GameCenterHelper, match: GKMatch?)
+}
+
+class GameCenterHelper: NSObject {
+    
+    static var shared = GameCenterHelper()
+    
+    var viewController: UIViewController?
+    
+    var delegate: GameCenterHelperDelegate?
+    
+    var isAuthenticated: Bool {
+        GKLocalPlayer.local.isAuthenticated
+    }
+    
+    override init() {
+        super.init()
+        GKLocalPlayer.local.authenticateHandler = { authVC, error in
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            
+            if let authVC = authVC {
+                self.delegate?.presentAuth(self, viewController: authVC)
+            }
+            GKLocalPlayer.local.register(self)
+            GKAccessPoint.shared.location = .topLeading
+            GKAccessPoint.shared.isActive = self.isAuthenticated
+        }
+    }
+    
+    func presentMatchmaker() {
+        guard GKLocalPlayer.local.isAuthenticated else {
+            return
+        }
+        
+        let request = GKMatchRequest()
+        request.defaultNumberOfPlayers = 2
+        request.inviteMessage = "Yuk main"
+        request.playerGroup = 2022
+        
+        let vc = GKMatchmakerViewController(matchRequest: request)!
+        vc.matchmakerDelegate = self
+        self.delegate?.presentMatchmaker(self, viewController: vc)
+    }
+}
+
+extension GameCenterHelper: GKMatchmakerViewControllerDelegate {
+    func matchmakerViewControllerWasCancelled(_ viewController: GKMatchmakerViewController) {
+        viewController.dismiss(animated: true)
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func matchmakerViewController(_ viewController: GKMatchmakerViewController, didFind match: GKMatch) {
+        viewController.dismiss(animated: true)
+        self.delegate?.presentGame(self, match: match)
+    }
+}
+
+extension GameCenterHelper: GKLocalPlayerListener {
+    func player(_ player: GKPlayer, didAccept invite: GKInvite) {
+        if let vc = GKMatchmakerViewController(invite: invite) {
+            vc.matchmakerDelegate = self
+            self.delegate?.presentMatchmaker(self, viewController: vc)
+        }
+    }
+}
--- GameCenterUIKit/ViewController.swift
+++ GameCenterUIKit/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
+    
+    var gameCenter = GameCenterHelper.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gameCenter.delegate = self
+        gameCenter.viewController = self
     }
+    
+    @IBAction func createRoom(_ sender: UIButton) {
+        gameCenter.presentMatchmaker()
+    }
+    
+    @IBAction func joinRoom(_ sender: UIButton) {
+    }
+}
 
+extension ViewController: GameCenterHelperDelegate {
+    func presentAuth(_ gameCenter: GameCenterHelper, viewController: UIViewController?) {
+        if let viewController = viewController {
+            self.present(viewController, animated: true)
+        }
+    }
+    
+    func presentMatchmaker(_ gameCenter: GameCenterHelper, viewController: UIViewController?) {
+        if let viewController = viewController {
+            self.present(viewController, animated: true)
+        }
+    }
+    
+    func presentGame(_ gameCenter: GameCenterHelper, match: GKMatch?) {
+        self.performSegue(withIdentifier: "goToGame", sender: match)
+    }
+}
 
+extension ViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToGame" {
+            if let controller = segue.destination as? GameViewController {
+                controller.match = sender as? GKMatch
+            }
+        }
+    }
 }
 
